8332449d749487fa19f4f56ff1b5ade7cca89ff4
odkhang <odkhang@tma.com.vn>
2024-10-25 15:22:51 +0700

    Fix isort in pipeline

--- src/pretix/control/views/organizer_views/organizer_view.py
+++ src/pretix/control/views/organizer_views/organizer_view.py
@@ -12,11 +12,7 @@ from django.utils.functional import cached_property
 from django.utils.translation import gettext_lazy as _
 from django.views import View
 from django.views.generic import (
-    CreateView,
-    DetailView,
-    FormView,
-    ListView,
-    UpdateView,
+    CreateView, DetailView, FormView, ListView, UpdateView,
 )
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -26,22 +22,16 @@ from pretix.base.models.organizer import Organizer, OrganizerBillingModel, Team
 from pretix.base.settings import SETTINGS_AFFECTING_CSS
 from pretix.control.forms.filter import EventFilterForm, OrganizerFilterForm
 from pretix.control.forms.organizer_forms import (
-    OrganizerDeleteForm,
-    OrganizerForm,
-    OrganizerSettingsForm,
+    OrganizerDeleteForm, OrganizerForm, OrganizerSettingsForm,
     OrganizerUpdateForm,
 )
 from pretix.control.permissions import (
-    AdministratorPermissionRequiredMixin,
-    OrganizerPermissionRequiredMixin,
+    AdministratorPermissionRequiredMixin, OrganizerPermissionRequiredMixin,
 )
 from pretix.control.signals import nav_organizer
 from pretix.control.utils import (
-    create_setup_intent,
-    get_payment_method_info,
-    get_stripe_customer_id,
-    get_stripe_publishable_key,
-    update_payment_info,
+    create_setup_intent, get_payment_method_info, get_stripe_customer_id,
+    get_stripe_publishable_key, update_payment_info,
 )
 from pretix.control.views import PaginationMixin
 from pretix.presale.style import regenerate_organizer_css
